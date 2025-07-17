58394c585d2c23be64432e3f52690c8be19c3586
Om Kumar <omku415@gmail.com>
2025-07-17 23:43:57 +0530

    initial setup

---
+++ server/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { connection } from "./database/dbConnection.js";
+
+export const app = express();
+config({ path: "./config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+connection();
