24ca2d3979fede717f877051d1e5c77471522f05
Om Kumar <omku415@gmail.com>
2025-07-28 10:45:15 +0530

    created Home and footer

--- client/src/layout/Footer.jsx
+++ client/src/layout/Footer.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "../styles/Footer.css";
 import { Link } from "react-router-dom";
-import fb from "../assets/fb.png";
 import yt from "../assets/yt.png";
 import git from "../assets/git.png";
 import linkedin from "../assets/linkedin.png";
+import leetcode from "../assets/leetcode.png";
 
 const Footer = () => {
   return (
@@ -12,45 +12,38 @@ const Footer = () => {
       <div className="footer-container">
         <div className="footer-logo">
           <h2>MERN Authentication</h2>
-          <p>Your ultimate guide to mastering the MERN stack.</p>
+          <p>A secure authentication system built using the MERN stack.</p>
         </div>
         <div className="footer-social">
           <h3>Follow Me</h3>
           <div className="social-icons">
             <Link
-              to="https://facebook.com/profile.php?id=100030535123397&mibextid=9R9pXO"
-              target="_blank"
-              className="social-link"
-            >
-              <img src={fb} alt="Facebook" />
-            </Link>
-            <Link
-              to="https://www.youtube.com/channel/UCbGtkGZ9sDg54PtU3GEDE6w"
-              target="_blank"
-              className="social-link"
-            >
-              <img src={yt} alt="Twitter" />
-            </Link>
-            <Link
-              to="https://www.linkedin.com/in/muhammad-zeeshan-khan-dev/"
+              to="https://www.linkedin.com/in/kumar-om45/"
               target="_blank"
               className="social-link"
             >
               <img src={linkedin} alt="LinkedIn" />
             </Link>
             <Link
-              to="https://github.com/Zeeshu911"
+              to="https://github.com/omku415"
               target="_blank"
               className="social-link"
             >
               <img src={git} alt="GitHub" />
             </Link>
+            <Link
+              to="https://leetcode.com/u/omku45/"
+              target="_blank"
+              className="social-link"
+            >
+              <img src={leetcode} alt="LeetCode" />
+            </Link>
           </div>
         </div>
       </div>
       <div className="footer-bottom">
-        <p>&copy; 2024 MERN Authentication. All Rights Reserved.</p>
-        <p>Designed by CodeWithZeeshu</p>
+        <p>&copy; 2025 MERN Authentication. All Rights Reserved.</p>
+        <p>Designed by Om Kumar</p>
       </div>
     </footer>
   );
